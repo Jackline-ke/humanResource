49211b6ca450448ccfe2fc2109266f9a1acde288
Jackline Adhiambo <antoninahodhiambo@gmail.com>
2020-09-13 15:32:53 +0300

    initial commit

---
+++ src/humanresource/HumanResource.java
@@ -0,0 +1,17 @@
+
+package humanresource;
+
+/**
+ *
+ * @author jackline
+ */
+public class HumanResource {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
